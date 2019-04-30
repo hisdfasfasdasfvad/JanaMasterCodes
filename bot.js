71cd17704f54ddfd428485c3d8fa6e534986c126
hisdfasfasdasfvad <46384271+hisdfasfasdasfvad@users.noreply.github.com>
2019-04-30 23:29:48 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,36 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', function(){
+    var ms = 10000 ;
+    var setGame = [' Jana','Welcome',' To',' Master Codes ','  رمضان كريم'];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/iXMiDo`);
+    }, ms);
+  console.log('---------------');
+  console.log('Jana Is Online')
+  console.log('---------------')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.Token);
